28f0bf5f5586fb3fdc541dabfdc7437077b4f7eb
Cleiton Hoffmann <cleiton_hoffmann@hotmail.com.br>
2024-06-13 23:29:25 -0300

    add classes to the dependency injection

--- ImageProcessing Functions/ImageProcessingFunctionApp/Program.cs
+++ ImageProcessing Functions/ImageProcessingFunctionApp/Program.cs
@@ -1,11 +1,18 @@
 using ImageProcessingFunctionApp.Contracts.Services;
 using ImageProcessingFunctionApp.Contracts.Steps;
+using ImageProcessingFunctionApp.Options;
 using ImageProcessingFunctionApp.Services;
 using ImageProcessingFunctionApp.Steps;
 using ImageProcessingFunctionApp.Steps.BlobUpload;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Configuration;
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", false)
+    .Build();
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
@@ -14,11 +21,12 @@ var host = new HostBuilder()
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
 
+        services.Configure<ImageSourceOptions>(configuration.GetSection(ImageSourceOptions.OPTIONS_NAME));
+
         services.AddScoped<IBlobStorageService, BlobStorageService>();
 
-        services.AddScoped<IProcessStepBuilderCreator, ProcessStepBuilderCreator>();
-
         services
+            .AddScoped<IProcessStepBuilderCreator, ProcessStepBuilderCreator>()
             .AddScoped<IProcessStepBuilder, BlobUploadProcessStepBuilder>();
 
 
